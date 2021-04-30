f44290aa155e19a28bd814b4938f3d00ed37cf2d
GuillermoMancilla <g.andres.mancilla@gmail.com>
2021-04-30 17:34:59 -0400

    ver 1.0 primer push juego web con angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dadosapp';
+  
+  dadoIzquierda = '../assets/images/dice1.png'
+  dadoDerecha = '../assets/images/dice4.png'
+  numero1 = 1;
+  numero2 = 2;
+
+  tirarDados():void{
+    this.numero1 = Math.round(Math.random()*5)+1;
+    this.numero2 = Math.round(Math.random()*5)+1;
+    this.dadoIzquierda = '../assets/images/dice' + this.numero1 + '.png';
+    this.dadoDerecha = '../assets/images/dice' + this.numero2 + '.png';
+  }
 }
